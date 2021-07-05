14851a0103a8ad33d6db24fed4aaf58cab28f4f2
Martin Pecánek <mates1500@gmail.com>
2021-07-05 21:15:08 +0200

    Add project files.

---
+++ game-man/game-man.cpp
@@ -0,0 +1,13 @@
+﻿// game-man.cpp : Defines the entry point for the application.
+//
+
+#include "game-man.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	scanf_s("%s");
+	return 0;
+}
